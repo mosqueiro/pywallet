6aa2355bd84a37fd1cd1e0b016eb2eade68b1351
Mosqueiro <erickcosta2010@gmail.com>
2020-07-22 02:35:16 -0300

    zcore

---
+++ zcore.py
@@ -0,0 +1,10 @@
+# zcore.py
+from pywallet import wallet
+
+# generate 12 word mnemonic seed
+seed = wallet.generate_mnemonic()
+
+# create bitcoin wallet
+w = wallet.create_wallet(network="ZCR", seed=seed, children=1)
+
+print(w)
